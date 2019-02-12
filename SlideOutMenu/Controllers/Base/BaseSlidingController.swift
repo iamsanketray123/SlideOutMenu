1908aa006c884bb7acc7b2226f4978a329f84fe8
xavier-castro <xavier_castro@icloud.com>
2019-02-12 00:53:05 -0600

    Add gesture in BadeSlidingController

--- SlideOutMenu/Controllers/Base/BaseSlidingController.swift
+++ SlideOutMenu/Controllers/Base/BaseSlidingController.swift
@@ -16,21 +16,40 @@ class BaseSlidingController: UIViewController {
         v.translatesAutoresizingMaskIntoConstraints = false
         return v
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .yellow
         
+        setupViews()
+        
+        // Translate our red view
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        let x = translation.x
+        
+        redViewLeadingConstraint.constant = x
+    }
+    
+    var redViewLeadingConstraint: NSLayoutConstraint!
+    
+    fileprivate func setupViews() {
         view.addSubview(redView)
         
         // Let's go ahead and use autolayout
         NSLayoutConstraint.activate([
             redView.topAnchor.constraint(equalTo: view.topAnchor),
-            redView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             redView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             redView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
             ])
+        
+        self.redViewLeadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
+        redViewLeadingConstraint.isActive = true
     }
-
+    
 }
